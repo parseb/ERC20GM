1dfae3e0bc48cdae47887e39b68ecb0956a7d622
parseb <petra306@protonmail.com>
2023-04-20 17:25:05 +0300

    signal init

--- src/ERC20GM.sol
+++ src/ERC20GM.sol
@@ -12,6 +12,11 @@ contract ERC20GM is ERC20, IERC20GM {
 
     uint64 price;
 
+
+    //// @notice constructor function instantiates immutable contract instance
+    //// @param name_ wanted name of token
+    //// @param symbol_ wanted symbol of token
+    //// @param price_ wanted initial price
     constructor(string memory name_, string memory symbol_, uint64 price_) ERC20(name_, symbol_) {
         price = price_ == 0 ? uint64(uint160(bytes20(address(this))) % 1 gwei) : price_;
     }
--- src/IERC20GM.sol
+++ src/IERC20GM.sol
@@ -4,5 +4,9 @@ pragma solidity 0.8.19;
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 interface IERC20GM is IERC20 {
+
+
+    //// @note signals preference and returns in-force pricing
+    function signal(uint64 price_) external returns (uint64);
     
 }
