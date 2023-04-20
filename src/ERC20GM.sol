a6fc3c02ffa235c7aa9926c17fc05e21701f188f
parseb <petra306@protonmail.com>
2023-04-21 02:53:18 +0300

    price base fx

--- src/ERC20GM.sol
+++ src/ERC20GM.sol
@@ -6,18 +6,63 @@ import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract ERC20GM is ERC20, IERC20GM {
     
-    /// Override
-    string private _name;
-    string private _symbol;
+    uint256 public price;
 
-    uint64 price;
+    //// price -> signal 
+    mapping(uint256 => uint256) priceSignal;
+
+    //// agent -> sigID : sigStrength
+    mapping(address => uint256[2]) agentSignal;
+
+
+
+    ////////////////// Events
+
+    event PriceChanged(uint256 price);
+
+    event PriceSignaled(address, uint256 indexed price, uint256 atTimeStamp);
+
+
+
+    ////////////////// External
 
 
     //// @notice constructor function instantiates immutable contract instance
     //// @param name_ wanted name of token
     //// @param symbol_ wanted symbol of token
     //// @param price_ wanted initial price
-    constructor(string memory name_, string memory symbol_, uint64 price_) ERC20(name_, symbol_) {
-        price = price_ == 0 ? uint64(uint160(bytes20(address(this))) % 1 gwei) : price_;
+    constructor(string memory name_, string memory symbol_, uint256 price_) ERC20(name_, symbol_) {
+        price = price_ == 0 ? uint256(uint160(bytes20(address(this))) % 1 gwei) : price_;
     }
+
+    //// @inheritdoc IERC20GM
+    function mint(uint256 howMany_) payable external returns (bool) {}
+
+    //// @inheritdoc IERC20GM
+    function signal(uint256 price_) external returns (uint256) {
+        uint256 pre = agentSignal[msg.sender][0];
+        if (pre > 0) priceSignal[pre] -= agentSignal[msg.sender][1];
+        agentSignal[msg.sender][0] = price_;
+        agentSignal[msg.sender][1] = balanceOf(msg.sender);
+        priceSignal[pre] += balanceOf(msg.sender);
+
+        if ( priceSignal[pre] > totalSupply() / 2 ) {
+             price = price_;
+             delete priceSignal[pre];
+             delete agentSignal[msg.sender][0];
+
+             emit PriceChanged(price);
+        }
+
+        emit PriceSignaled(msg.sender, price_, block.timestamp);
+        return price;
+    }
+
+
+    ////////////////// VIEW //////////////////
+
+    //// @inheritdoc IERC20GM
+    function howMuchFor(uint256 howMany_) external view returns (uint256)  { 
+        return ( howMany_ * price); 
+        }
 }
--- src/IERC20GM.sol
+++ src/IERC20GM.sol
@@ -6,7 +6,18 @@ import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
 interface IERC20GM is IERC20 {
 
 
-    //// @note signals preference and returns in-force pricing
-    function signal(uint64 price_) external returns (uint64);
+    //// @notice signals preference and returns current in-force price
+    //// @param price_ prefered price amount in gwei
+    function signal(uint256 price_) external returns (uint256);
+
+
+    //// @notice mints specified amount to msg.sender requires corresponding value
+    //// @param howMany_ number of tokens wanted
+    function mint(uint256 howMany_) payable external returns (bool);
+
+    //// @notice calculates how much specified howMany costs for value sent 
+    //// @param howMany_ how many
+    //// @return returns total value of how many times price
+    function howMuchFor(uint256 howMany_) external view returns (uint256);
     
 }
